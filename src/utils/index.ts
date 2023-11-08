e053cda5202e45b9bee3024d68ce52d9bd5b087c
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-11-08 23:56:36 +0100

    wip

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
-export * from "./fetch.js";
 export * from "./functional.js";
 export * from "./method.js";
 export * from "./parameters.js";
 export * from "./status-code.js";
+export * from "./stream.js";
